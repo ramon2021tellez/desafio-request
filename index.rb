91fe25c841e9ca9a8b9f2f2eb32582751cb7c69e
ramon2021tellez <101756322+ramon2021tellez@users.noreply.github.com>
2022-04-12 15:12:50 -0400

    commit original

--- index.rb
+++ index.rb
@@ -13,20 +13,32 @@ def request(url)
     response = https.request(request)
     results = JSON.parse(response.read_body)
 end
-# limitamos la respuesta a 10 registros
-data = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=UUwWiOoisWanWwJR7hLGZINRsSk46Lv0bU86KS60")
-# recorremos la respuesta y obetnemos las "thumbnailUrl"
-#photos = data.map do |photo|
-#    photo["img_src"]
-#end
+# descargamos la informacion en la variable data
+data = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=2&api_key=UUwWiOoisWanWwJR7hLGZINRsSk46Lv0bU86KS60")
 
+#seleccionamos las fotos en la variable picture
+picture = data['photos']
 html = ""
-data.each do |photo|
+
+
     html += "<html>\n"
-    html += "<img src='#{photo}'>\n"
-    html += "<img src='https://mars.nasa.gov/msl-raw-images/msss/01000/mcam/398381687EDR_F0030000CCAM05010M_.JPG'>\n" 
-end
+    html += "<head>\n"
+    html += "</head>\n"
+    html += "<body>\n"
+    #cuenta la cantidad de fotos la variable photos_count
+    photos_count = picture.size
+    html += "<p> Cantidad de fotos :#{photos_count}</p>\n "
+    html += "</ul>\n"  
+    #recorremos el hash mostrando las imagenes 
+    picture.each do |photo|
+        html += "<li><img src = '#{photo['img_src']}' width='300' height= '300' class='img-fluid rounded'><li>\n"
+        html += "<p> '#{photo['camera.full_name']}'</p>\n "
+    end  
+    html += "</ul>\n"   
+    html += "</body>\n"
+    html += "</html>\n"
+   
+   #generamos en index con la estructura html 
+   
+    File.write('index.html', html)
 
-File.write('index.html', html)
-
-#puts buid_web_page("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=UUwWiOoisWanWwJR7hLGZINRsSk46Lv0bU86KS60")[0..9]
